9d4b890fbf8cc954db5f59eed51b00eaafb2f578
volodymyr bondariev <vladmikhbond@hotmail.com>
2020-10-22 22:05:51 +0300

    StaffController

---
+++ Dept2/Data/Migrations/20201022185119_Persons.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dept2.Data.Migrations
+{
+    public partial class Persons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Resume",
+                table: "Persons");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Link",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name_en",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name_ru",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name_uk",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rank",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resume_en",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resume_ru",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resume_uk",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Link",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Name_en",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Name_ru",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Name_uk",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Rank",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Resume_en",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Resume_ru",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Resume_uk",
+                table: "Persons");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Persons",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resume",
+                table: "Persons",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+        }
+    }
+}
--- Dept2/Models/Person.cs
+++ Dept2/Models/Person.cs
@@ -12,9 +12,16 @@ namespace Dept2.Models
    {
       public int Id { set; get; }
       [Required]
-      [MaxLength(100)]
-      public string Name { set; get; }
-      [MaxLength(1000)]
-      public string Resume { set; get; }
+      public string Name_uk { set; get; }
+      public string Name_ru { set; get; }
+      public string Name_en { set; get; }
+      [Required]
+      public string Resume_uk { set; get; }
+      public string Resume_ru { set; get; }
+      public string Resume_en { set; get; }
+
+      public int Rank { set; get; }          // 1-сотр  2-асс  3-преп  4-доц  5-проф      
+      public string Link { set; get; }       // for httр://nure.ua/staff/Link
+      public string ImageName { set; get; }  // for /pic/staff/ImageName
    }
 }
